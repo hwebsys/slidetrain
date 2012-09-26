58e24a9419e8a36714454714bf7a4825c13f380f
Tomohiko Himura <eiel.hal@gmail.com>
2012-09-26 17:06:36 +0900

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slidetrain::Application.config.secret_token = '43e68d4758f5b5e0455a03a21f18029710567d430db5b96e21e882eaa30053c6feccce68f3381e3fce180456f3ad6a4b716005828c0c4897c07b76af997e0f7e'
